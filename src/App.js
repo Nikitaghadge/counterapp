d7af2aec35f69c7b830da060da29147a757f7fe9
unknown <ghadgenikita91@gmail.com>
2023-12-25 13:21:24 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrease = () => {
+    setCount((prevCount) => Math.max( prevCount - 1));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="max-w-md mx-auto p-6 bg-gray-100 border rounded-md shadow-md">
+        <h1 className="text-3xl font-bold mb-4 text-center">Counter App</h1>
+        <h2 className="text-lg mb-4 text-center">Count: {count}</h2>
+        <button
+          className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mr-2"
+          onClick={handleIncrease}
         >
-          Learn React
-        </a>
-      </header>
+          Increment
+        </button>
+        <button
+          className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+          onClick={handleDecrease}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
